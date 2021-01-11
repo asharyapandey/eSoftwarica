aab0884b96c347094e79ab2a84e902fec529500a
asharyapandey <asharyap7@gmail.com>
2021-01-11 18:03:30 +0545

    added fragments

--- app/src/main/java/com/asharya/esoftwarica/DashboardActivity.kt
+++ app/src/main/java/com/asharya/esoftwarica/DashboardActivity.kt
@@ -2,10 +2,30 @@ package com.asharya.esoftwarica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.asharya.esoftwarica.fragments.AboutFragment
+import com.asharya.esoftwarica.fragments.AddStudentFragment
+import com.asharya.esoftwarica.fragments.HomeFragment
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val homeFragment = HomeFragment()
+        val aboutFragment = AboutFragment()
+        val AddStudentFragment = AddStudentFragment()
+
+        setFragment(homeFragment)
+
+        val bottomNaviga
+
     }
+
+    private fun setFragment(fragment : Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            addToBackStack(null)
+            commit()
+        }
 }
