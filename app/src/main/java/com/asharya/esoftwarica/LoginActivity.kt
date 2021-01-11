2798c7c68a9f3472fb8940f636401358d091d77d
asharyapandey <asharyap7@gmail.com>
2021-01-11 17:33:27 +0545

    login activity done

---
+++ app/src/main/java/com/asharya/esoftwarica/LoginActivity.kt
@@ -0,0 +1,42 @@
+package com.asharya.esoftwarica
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var etUsername: TextInputEditText
+    private lateinit var etPassword: TextInputEditText
+    private lateinit var btnLogin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+
+        btnLogin.setOnClickListener {
+            if (validate()) {
+                val username = etUsername.text.toString().trim()
+                val password = etPassword.text.toString().trim()
+
+                if (username == "softwarica" && password == "coventry") {
+                    Intent(this, DashboardActivity::class.java).also {
+                        startActivity(it)
+                    }
+                } else {
+                    Toast.makeText(this, "Either username or password is incorrect.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+
+}
