081f6b4943a14633641a44ed62147f37def20cd5
asharyapandey <asharyap7@gmail.com>
2021-01-11 19:14:35 +0545

    added student model

---
+++ app/src/main/java/com/asharya/esoftwarica/models/Student.kt
@@ -0,0 +1,9 @@
+package com.asharya.esoftwarica.models
+
+data class Student(
+    val fullName : String,
+    val age : Int,
+    val gender : String,
+    val address : String,
+    val profilePicture : String
+)
